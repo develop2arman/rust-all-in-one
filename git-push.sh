35beaf8d226ae0738ea323726b146bce3c3f7172
Arman Riazi <arman2riazi@gmail.com>
2022-12-16 21:50:14 +0000

    test6

--- git-push.sh
+++ git-push.sh
@@ -13,7 +13,7 @@ git commit -m "${message}"
 #
 
  echo "Pushing data to remote server!!!"
- if ${branch} == ""
+ if [[ -z ${branch} ]]; then
     git push -u origin main
  else
     git push -u origin "${branch}"
