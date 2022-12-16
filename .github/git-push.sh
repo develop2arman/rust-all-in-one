c7028f301e1c92e59cc2e426286c91a6c7203956
Arman Riazi <arman2riazi@gmail.com>
2022-12-16 21:31:37 +0000

    edit git f

--- .github/git-push.sh
+++ .github/git-push.sh
@@ -1,5 +1,7 @@
 echo "Enter your message"
 read message
+ echo 'Enter the name of the branch:'
+ read branch
 git add .
 git commit -m"${message}"
 if [ -n "$(git status - porcelain)" ];
@@ -8,7 +10,5 @@ then
 else
  git status
  echo "Pushing data to remote server!!!"
- echo 'Enter the name of the branch:'
- read branch
- git push -u origin $branch
+ git push -u origin "${branch}"
 fi
