cf8b01aed53a197378324e0b51586255508ee7bb
Arman Riazi <arman2riazi@gmail.com>
2023-01-11 09:35:08 +0000

    commited

--- git-push.sh
+++ git-push.sh
@@ -1,10 +1,13 @@
-git status
+
 echo "Enter your message"
 read message
 #echo 'Enter the name of the branch:'
 #read branch
-git add --all
-git commit -m "${message}"
+# git add --all
+
+echo "git added all"
+
+git commit -a "${message}"
 
 #if [ -n "$(git status - porcelain)" ];
 #then
@@ -12,6 +15,10 @@ git commit -m "${message}"
 #else
 #
 
- echo "Pushing data to remote server!!!"
+echo "Pushing data to remote server!!!"
 
- git push -u origin main
+git push -u origin main
+
+echo "Pushed data successfully"
+
+git status
