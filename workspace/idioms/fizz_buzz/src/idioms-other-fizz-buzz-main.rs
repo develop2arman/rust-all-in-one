0dcfcfbd50d9b5dab00d611c8387b01e20ba4ec4
armanriazi <arman2riazi@gmail.com>
2023-03-28 21:29:13 +0000

    change ad

---
+++ workspace/idioms/fizz_buzz/src/idioms-other-fizz-buzz-main.rs
@@ -0,0 +1,46 @@
+#![allow(dead_code, unused_variables)]
+
+/// Main
+///
+/// ## Commands
+///
+/// ```cargo run -q -p idioms-other-fizz-buzz_bin --bin idioms-other-fizz-buzz-main```
+///
+/// ## What
+/// `loop int`
+///
+/// ## How
+/// `TODO`
+///
+/// # Arguments
+///
+/// * `Arg1` - This is the [your type] to [your verb] the [your struct/func name]
+///
+/// # Return
+/// `unimplemented`
+///
+/// ## Example
+///```rust
+
+// struct Solution;
+
+// impl Solution {
+//     pub fn fizz_buzz(n: i32) -> Vec<String> {
+//         let answer: Vec<i32>= (1..&n)[]
+//         if (i%3==0){
+//         answer[i] == "FizzBuzz"
+//         }
+//         else if (i%5==0){
+//         answer[i] == "Buzz"
+//         }
+//         else{
+//         answer[i] == i.to_string();
+//         }
+//     }
+// }
+
+
+fn main(){
+    //Solution::fizz_buzz(5);
+    unimplemented!();
+}
