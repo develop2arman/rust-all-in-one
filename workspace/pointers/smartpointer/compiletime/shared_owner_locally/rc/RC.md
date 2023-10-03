0c4103229b433fb1b8bce55112748a7a744dbb91
armanriazi <armanriyazi.github.io@hotmail.com>
2023-10-03 23:03:42 +0000

    rc added doc weak strong

--- workspace/pointers/smartpointer/compiletime/shared_owner_locally/rc/RC.md
+++ workspace/pointers/smartpointer/compiletime/shared_owner_locally/rc/RC.md
@@ -92,6 +92,15 @@ With [[interior_mutability]], you may want to provide an argument to a method th
 > If implementing Clone would be prohibitively expensive, Rc<T> can be a handy alternative. This allows two places to “share” ownership.
 > > **Rc<T> is not thread-safe. In multithreaded code**, it’s much better to replace Rc<T> with Arc<T> and Rc<RefCell<T>> with Arc<Mutex<T>>. [[Arc]] stands for #atomic reference counter.
 
+## Week
+
+Weak is a version of Rc that **holds a non-owning reference** to the managed allocation. The allocation is accessed by calling upgrade on the Weak pointer, which returns an Option<Rc<T>>.
+
+Since a Weak reference does not count towards ownership, it will not prevent the value stored in the allocation from being dropped, and Weak itself makes no guarantees about the value still being present. Thus it may return None when upgraded. Note however that a Weak reference does prevent the allocation itself (the backing store) from being deallocated.
+
+A Weak pointer is useful for keeping a temporary reference to the allocation managed by Rc without preventing its inner value from being dropped. It is also **used to prevent circular references between Rc pointers**, since mutual owning references would never allow either Rc to be dropped. For example, a tree could have **strong Rc pointers from parent nodes to children, and Weak pointers from children back to their parents.**
+
+The typical way to obtain **a Weak pointer is to call Rc::downgrade**
 
 
 ---
--- workspace/pointers/smartpointer/compiletime/shared_owner_locally/rc/rust-doc/src/bin/rust-doc-rc-ex-1.rs
+++ workspace/pointers/smartpointer/compiletime/shared_owner_locally/rc/rust-doc/src/bin/rust-doc-rc-ex-1.rs
@@ -4,7 +4,7 @@
 ///
 /// ## Commands
 ///
-/// ```cargo run -q -p rust-doc-rc_bin --bin rust-doc-rc-main```
+/// ```cargo run -q -p rust-doc-rc_bin --bin rust-doc-rc-ex-1```
 ///
 /// ```cargo doc  --package rust-doc-rc_bin  --message-format short --no-deps --open --color always```
 ///
@@ -36,7 +36,7 @@ enum List {
 
 use crate::List::{Cons, Nil};
 use std::rc::Rc;
-
+/*
 fn main() {
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
     println!("count after creating a = {}", Rc::strong_count(&a));
@@ -47,24 +47,28 @@ fn main() {
         println!("count after creating c = {}", Rc::strong_count(&a));
     }
     println!("count after c goes out of scope = {}", Rc::strong_count(&a));
-}
-/*We could have called a.clone() rather than Rc::clone(&a), but Rust’s convention is to use Rc::clone in this case. The implementation of Rc::clone doesn’t make a deep copy of all the data like most types’ implementations of clone do. The call to Rc::clone only increments the reference count, which doesn’t take much time.
+}*/
+/*We could have called a.clone() rather than Rc::clone(&a), but Rustâ€™s convention is to use Rc::clone in this case. The implementation of Rc::clone doesnâ€™t make a deep copy of all the data like most typesâ€™ implementations of clone do. The call to Rc::clone only increments the reference count, which doesnâ€™t take much time.
  When c goes out of scope, the count goes down by 1-above
 he implementation of the Drop trait decreases the reference count automatically when an Rc<T> value goes out of scope.
 
 
-
+*/
 
 fn main() {
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let da=Rc::downgrade(&a);
     println!("count after creating a = {}", Rc::weak_count(&a));
+    let da=Rc::downgrade(&a);
     let b = Cons(3, Rc::clone(&a));
     println!("count after creating b = {}", Rc::weak_count(&a));
-    {
+    {   let da=Rc::downgrade(&a);
         let c = Cons(4, Rc::clone(&a));
         println!("count after creating c = {}", Rc::weak_count(&a));
+         //
     }
     println!("count after c goes out of scope = {}", Rc::weak_count(&a));
+     let da=Rc::downgrade(&a);
 }
 
-*/
+
